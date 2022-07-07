0b2f753033f7c789a7f502bfc2a51ae5dfaac041
CyskoFR <franc.sko91@gmail.com>
2022-07-07 16:32:14 +0200

    add base scaffolding

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.base')
+
+@section('page-title')
+    Home
+@endsection
+
+@section('page-content')
+    
+@endsection
---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="header_container">
+        <div class="header_logo">
+            <img src="" alt="dc-logo.png">
+        </div>
+        <nav class="header_nav">
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+</header>
