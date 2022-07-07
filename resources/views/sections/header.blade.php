a60a2c892ee38fcb587dfcb3a535c2d092ef0129
CyskoFR <franc.sko91@gmail.com>
2022-07-07 16:56:47 +0200

    add partials

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="header_container">
         <div class="header_logo">
-            <img src="" alt="dc-logo.png">
+            <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo.png">
         </div>
         <nav class="header_nav">
             <ul>
