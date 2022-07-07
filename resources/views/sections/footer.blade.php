661401cae1152ff5bc093818a2e735b88230e7b4
CyskoFR <franc.sko91@gmail.com>
2022-07-07 18:09:48 +0200

    add footer

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,55 @@
+<footer>
+    <div class="footer_up-section_container">
+        <div class="footer_up-section">
+            <nav>
+                <ul>DC COMICS
+                    <li><a href="#">Characters</a></li>
+                    <li><a href="#">Comics</a></li>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Games</a></li>
+                    <li><a href="#">Videos</a></li>
+                    <li><a href="#">News</a></li>
+                </ul>
+                <ul>SHOP
+                    <li><a href="#">Shop DC</a></li>
+                    <li><a href="#">Shop DC collectibles</a></li>
+                </ul>
+                <ul>DC
+                    <li><a href="#">Terms Of Use</a></li>
+                    <li><a href="#">Privacy Policy (New)</a></li>
+                    <li><a href="#">Ad Choices</a></li>
+                    <li><a href="#">Advertising</a></li>
+                    <li><a href="#">Jobs</a></li>
+                    <li><a href="#">Subscriptions</a></li>
+                    <li><a href="#">Talent Workshops</a></li>
+                    <li><a href="#">CPSC Certificates</a></li>
+                    <li><a href="#">Ratings</a></li>
+                    <li><a href="#">Shop Help</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+                <ul>SITES
+                    <li><a href="#">DC</a></li>
+                    <li><a href="#">MAD Magazine</a></li>
+                    <li><a href="#">DC Kids</a></li>
+                    <li><a href="#">DC Universe</a></li>
+                    <li><a href="#">DC Power Visa</a></li>
+                </ul>
+            </nav>
+            <img class="dc-logo-bg" src="{{asset('img/dc-logo-bg.png')}}" alt="dc-logo-bg.png">
+        </div>
+    </div>
+    <div class="footer_down-section_container">
+        <div class="footer_down-section">
+            <a class="footer_button" href="#">SIGN-UP NOW!</a>
+            <div class="social-box">
+                <a href="#">FOLLOW US</a>
+                <a href="#"><img src="{{asset('img/footer-facebook.png')}}" alt="footer-facebook.png"></a>
+                <a href="#"><img src="{{asset('img/footer-twitter.png')}}" alt="footer-twitter.png"></a>
+                <a href="#"><img src="{{asset('img/footer-youtube.png')}}" alt="footer-youtube.png"></a>
+                <a href="#"><img src="{{asset('img/footer-pinterest.png')}}" alt="footer-pinterest.png"></a>
+                <a href="#"><img src="{{asset('img/footer-periscope.png')}}" alt="footer-periscope.png"></a>
+            </div>
+        </div>
+    </div>
+</footer>
